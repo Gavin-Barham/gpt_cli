9d524b7d71e6c6728888849747e2fc08f82a3183
Gavin Barham <106175633+Gavin-Barham@users.noreply.github.com>
2023-05-12 21:45:59 -0500

    Update gpt.zsh

--- gpt.zsh
+++ gpt.zsh
@@ -5,11 +5,11 @@ current_dir=$(pwd)
 
 # Change directory to the script location
 #update this with your path to this directory
-cd /Users/gavinbarham/Documents/my-code/repos/gpt_cli
+cd {path to this directory}
 
 # Run the Node.js script with absolute path
 # Update this with your path to this directory followed by the index.js
-node /Users/gavinbarham/Documents/my-code/repos/gpt_cli/index.js
+node {path to this directory}/index.js
 
 # Restore the original working directory
 cd "$current_dir"
